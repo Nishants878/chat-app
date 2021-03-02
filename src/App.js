295a4b62828fd396de15d3ec3ec589e13631961b
Nishants878 <nishants878@gmail.com>
2021-03-02 14:43:30 +0530

    inital build intiated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css'
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChatEngine
+    height='100vh'
+    userName='krool_horse'
+    userSecret='1234'
+    projectID='9eec610c-070a-4c97-ad46-c2a06003d32d'
+  />
+  )
 }
-
-export default App;
