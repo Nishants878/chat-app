23cde5c0d1e3515db5eeb7ccf2e535c7564c3ed0
Nishants878 <nishants878@gmail.com>
2021-03-02 19:55:34 +0530

    added chatfeed and message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css'
-
+import ChatFeed from './components/ChatFeed';
 
 
 export default function App() {
@@ -11,6 +11,7 @@ export default function App() {
     userName='krool_horse'
     userSecret='1234'
     projectID='9eec610c-070a-4c97-ad46-c2a06003d32d'
+    renderChatFeed={(chatAppProps) => <ChatFeed  { ...chatAppProps } />}
   />
   )
 }
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export default function MessageForm() {
+
+    const [value, setValue] = useState('');
+
+     const handleSubmit = ()=>{
+
+     };
+
+     const handleChange = ()=>{
+
+    }
+
+    return (
+        <form className="message-form" onSubmit={handleSubmit}>
+              <input 
+                 className='message-input'
+                 placeholder='Send a message...'
+                 value={value} 
+                 onChange={handleChange}
+                 onSubmit={handleSubmit}
+              />
+        </form>
+    )
+}
