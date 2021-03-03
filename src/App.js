fe17b637ad7a8e1189ff2301df30ae9fa1421b53
Nishants878 <nishants878@gmail.com>
2021-03-03 15:20:10 +0530

    app built (basic css only)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './components/LoginForm';
 import './App.css'
 import ChatFeed from './components/ChatFeed';
 
+const projectID = '9eec610c-070a-4c97-ad46-c2a06003d32d'
 
 export default function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  
   return (
     <ChatEngine
-    height='100vh'
-    userName='krool_horse'
-    userSecret='1234'
-    projectID='9eec610c-070a-4c97-ad46-c2a06003d32d'
-    renderChatFeed={(chatAppProps) => <ChatFeed  { ...chatAppProps } />}
+    height="100vh"
+    projectID={projectID}
+    userName={localStorage.getItem('username')}
+    userSecret={localStorage.getItem('password')}
+    renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
   />
   )
 }
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,16 +1,30 @@
 import React, {useState} from 'react'
+import { sendMessage, isTyping  } from 'react-chat-engine'; 
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
-export default function MessageForm() {
+export default function MessageForm(props) {
 
     const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
-     const handleSubmit = ()=>{
+     const handleSubmit = (event)=>{
+        event.preventDefault();
 
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds,chatId, { text } ) 
+
+        setValue('');
      };
 
-     const handleChange = ()=>{
-
+     const handleChange = (event)=>{
+         setValue(event.target.value);
+         isTyping(props, chatId)
     }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: '' });
+      };
 
     return (
         <form className="message-form" onSubmit={handleSubmit}>
@@ -21,6 +35,21 @@ export default function MessageForm() {
                  onChange={handleChange}
                  onSubmit={handleSubmit}
               />
+              <label htmlFor="upload-button">
+                  <span className="image-button">
+                  <PictureOutlined className="picture-icon" />
+                  </span>
+              </label>
+              <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload.bind(this)}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
         </form>
     )
 }
